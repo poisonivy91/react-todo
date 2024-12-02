508ca4a23905b1d4068d40240d62450b7307d1b2
Shaina Bowser <sbowser916@gmail.com>
2024-12-02 16:38:07 -0500

    Updated form functionality and connected state to list

--- react-todo/src/AddTodoForm.jsx
+++ react-todo/src/AddTodoForm.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 
+function AddTodoForm({ onAddTodo }) {
+  const handleAddTodo = (event) => {
+    event.preventDefault(); 
+    const todoTitle = event.target.title.value; 
+    if (todoTitle.trim() === "") {
+      alert("Todo cannot be empty!");
+      return;
+    }
+    onAddTodo(todoTitle); 
+    event.target.reset(); 
+  };
 
-function AddTodoForm(){
-    const handleAddTodo = (event) => {
-        event.preventDefault();
-        const todoTitle = event.target.title.value;
-        console.log(todoTitle);
-        event.target.reset();
-    };
-
-
-    return(
+  return (
     <form onSubmit={handleAddTodo}>
-        <label text="Title" htmlFor="todoTitle">Title</label>
-            <input type="text" id="todoTitle" name="title" />
-            <button type="submit">Add</button> 
+      <label htmlFor="todoTitle">Title</label>
+      <input type="text" id="todoTitle" name="title" />
+      <button type="submit">Add</button>
     </form>
-    );
+  );
 }
 
 export default AddTodoForm;
+
