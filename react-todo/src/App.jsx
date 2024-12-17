38d2df727085ea1364649f7a7056b3aa7cea309b
Shaina Bowser <sbowser916@gmail.com>
2024-12-16 20:46:54 -0500

    Completed wk 6 hooks / fragments assignments

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,13 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AddTodoForm from "./AddTodoForm";
 import TodoList from "./TodoList";
 
+
+function useSemiPersistentState(key, initialValue) {
+  const [value, setValue] = React.useState(() => {
+    const savedValue = localStorage.getItem(key);
+    return savedValue ? JSON.parse(savedValue) : initialValue;
+  });
+
+  React.useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+
+}
+
 function App() {
-  const [todoList, setTodoList] = useState([]); // Start with an empty list
+  const [todoList, setTodoList] = useSemiPersistentState("savedTodoList", []);
+  
 
   const addTodo = (newTodo) => {
-    setTodoList([...todoList, newTodo]); // Add the new todo to the list
+    setTodoList([...todoList, newTodo]); 
   };
+
+  // useEffect(() => {
+  //   localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+  // }, [todoList]);
 
   return (
     <div>
